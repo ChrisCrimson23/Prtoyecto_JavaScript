28996692aeba458585967dd35270bd1241c15b93
ChrisCrimson23 <christrabajo98otmail.com>
2023-08-08 22:06:26 -0600

    Prtoyecto_JavaScript

--- Proyecto_Final.js
+++ Proyecto_Final.js
@@ -1,27 +1,84 @@
-// Obtener todas las imágenes con la clase "Autos_1"
-const imagenesAutos = document.querySelectorAll('.Autos_1');
+document.addEventListener('DOMContentLoaded', function () {
+  function ocultarTodasLasImagenes(contenedor) {
+    const imagenes = contenedor.querySelectorAll('img');
+    imagenes.forEach((imagen) => {
+      imagen.style.display = 'none';
+    });
+  }
 
-// Inicializar el índice de la imagen actual en 0
-let indiceActual = 0;
+  function mostrarImagenSiguiente(contenedor, indiceActual) {
+    const imagenes = contenedor.querySelectorAll('img');
+    ocultarTodasLasImagenes(contenedor);
+    imagenes[indiceActual].style.display = 'block';
+    return (indiceActual + 1) % imagenes.length;
+  }
 
-// Función para mostrar la imagen siguiente
-function mostrarSiguienteImagen() {
-  // Ocultar todas las imágenes
-  imagenesAutos.forEach((imagen) => {
-    imagen.style.display = 'none';
+  const divAutos1 = document.querySelector('.Presentación .Descripción_Autos_1');
+  const divAutos2 = document.querySelector('.Presentación .Descripción_Autos_2');
+  const divAutos3 = document.querySelector('.Presentación .Descripción_Autos_3');
+
+  let indiceActual1 = 0;
+  let indiceActual2 = 0;
+  let indiceActual3 = 0;
+
+  divAutos1.addEventListener('click', () => {
+    indiceActual1 = mostrarImagenSiguiente(divAutos1, indiceActual1);
   });
 
-  // Mostrar la imagen actual
-  imagenesAutos[indiceActual].style.display = 'block';
+  divAutos2.addEventListener('click', () => {
+    indiceActual2 = mostrarImagenSiguiente(divAutos2, indiceActual2);
+  });
 
-  // Incrementar el índice para mostrar la siguiente imagen
-  indiceActual = (indiceActual + 1) % imagenesAutos.length;
-}
-
-// Agregar evento de clic a cada imagen
-imagenesAutos.forEach((imagen) => {
-  imagen.addEventListener('click', mostrarSiguienteImagen);
+  divAutos3.addEventListener('click', () => {
+    indiceActual3 = mostrarImagenSiguiente(divAutos3, indiceActual3);
+  });
 });
 
-// Mostrar la primera imagen al cargar la página
-mostrarSiguienteImagen();
+document.addEventListener('DOMContentLoaded', function () {
+    
+    document.getElementById('nombre').addEventListener('input', function (event) {
+      event.target.value = event.target.value.toUpperCase();
+    });
+  
+    document.getElementById('apellido_paterno').addEventListener('input', function (event) {
+      event.target.value = event.target.value.toUpperCase();
+    });
+  
+    document.getElementById('apellido_materno').addEventListener('input', function (event) {
+      event.target.value = event.target.value.toUpperCase();
+    });
+  
+    document.getElementById('cuestionarioForm').addEventListener('submit', function (event) {
+      event.preventDefault();
+  
+      let nombre = document.getElementById('nombre').value;
+      let apellido_paterno = document.getElementById('apellido_paterno').value;
+      let apellido_materno = document.getElementById('apellido_materno').value;
+      const numero = document.getElementById('numero').value;
+      const direccion = document.getElementById('direccion').value;
+      const edad = document.getElementById('edad').value;
+      const email = document.getElementById('email').value;
+      const tipoVehiculo = document.getElementById('tipoVehiculo').value;
+  
+      
+      nombre = nombre.toUpperCase();
+      apellido_paterno = apellido_paterno.toUpperCase();
+      apellido_materno = apellido_materno.toUpperCase();
+  
+      
+      if (edad < 18) {
+        alert('Perdón por las molestias, pero debido a que usted es aún menor de edad no puede continuar con su registro para usar nuestros servicios. Una disculpa.');
+        return; 
+      }
+  
+      console.log('Datos ingresados por el usuario:');
+      console.log('Nombre:', nombre);
+      console.log('Apellido Paterno:', apellido_paterno);
+      console.log('Apellido Materno:', apellido_materno);
+      console.log('Número de teléfono:', numero);
+      console.log('Dirección:', direccion);
+      console.log('Edad:', edad);
+      console.log('Correo electrónico:', email);
+      console.log('Tipo de vehículo:', tipoVehiculo);
+    });
+  });
